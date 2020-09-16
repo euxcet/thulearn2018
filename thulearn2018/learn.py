d4440284ef04edeee217e98cd488867bf1fd4040
thuliu-yt16 <lytlogic@gmail.com>
2020-09-16 21:39:38 +0800

    v2.1.7: now can show configuration

--- thulearn2018/learn.py
+++ thulearn2018/learn.py
@@ -20,6 +20,14 @@ def reset():
 	learn.set_user()
 	learn.set_path()
 
+@click.command(help = 'Show configurations.')
+def config():
+	username, _ = learn.get_user()
+	path = learn.get_path()
+	print('Username: %s' % username)
+	print('Path: %s' % path)
+
+
 @click.command(help = 'Clear records of all downloaded files.')
 def clear():
 	learn.set_local()
@@ -49,7 +57,7 @@ def align(string, length=0):
 @click.command(help = 'Show homework deadlines.')
 def ddl():
 	ddls = learn.get_ddl()
-	print(f'Total {len(ddls)} ddl(s)')
+	print('Total %d ddl(s)' % (len(ddls)))
 	for ddl in ddls:
 		print(align(ddl[0][0:8], 25), align(ddl[1][0:20], 30) + align(ddl[3][0:20], 30), ddl[4])
 
@@ -63,6 +71,7 @@ main.add_command(reset)
 main.add_command(clear)
 main.add_command(submit)
 main.add_command(ddl)
+main.add_command(config)
 
 
 if __name__ == "__main__":
