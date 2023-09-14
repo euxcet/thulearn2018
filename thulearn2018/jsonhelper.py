85ab32b398f334e36be2b5cc6cebd1c982dfc6fe
YuruiHong <yuruihong02@outlook.com>
2023-09-14 11:50:14 +0800

    fix minor bugs due to API change of Web Learning

--- thulearn2018/jsonhelper.py
+++ thulearn2018/jsonhelper.py
@@ -1,20 +1,16 @@
 import json
-import platform
+from re import findall
 
 class JsonHelper():
     def __init__(self):
         pass
 
     def loads(self, content):
-        def is_windows():
-            return platform.system() == 'Windows'
-            
         result = {}
         try:
-            if(is_windows()):
-                result = json.loads(bytes.decode(content))
-            else:
-                result = json.loads(content)
+            if isinstance(content, bytes):
+                content = bytes.decode(content)
+            result = json.loads(findall(r'({.*})', content)[0])
         except Exception:
             print("密码错误")
             exit(1)
