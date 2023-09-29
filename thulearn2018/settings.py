dd3cadaf9c237e46b3df3f130d18492bbc148d0e
YuruiHong <yuruihong02@outlook.com>
2023-09-29 23:03:35 +0800

    follow XDG standard

--- thulearn2018/settings.py
+++ thulearn2018/settings.py
@@ -1,23 +1,33 @@
-import tempfile, os
+import tempfile
+import os
 from pathlib import Path
 from urllib3 import encode_multipart_formdata
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
-headers = {'Connection': 'keep-alive', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36' }
+headers = {'Connection': 'keep-alive', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*',
+           'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36'}
 upload_headers = {'Connection': 'keep-alive', 'Accept-Encoding': 'gzip, deflate', 'Accept': '*/*', 'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Mobile Safari/537.36',
- 		'Content-Type':'multipart/form-data; boundary=----WebKitFormBoundaryTytyPd5kgvE3t0kW'}
+                  'Content-Type': 'multipart/form-data; boundary=----WebKitFormBoundaryTytyPd5kgvE3t0kW'}
 
-# url = "http://learn2018.tsinghua.edu.cn/"
 url = "https://learn.tsinghua.edu.cn/"
 
-user_file_name = '.thulearn2018-user.txt'
-local_file_name = '.thulearn2018-local.txt'
-path_file_name = '.thulearn2018-path.txt'
-temp_path = str(Path.home())
-#temp_path = tempfile.gettempdir()
-user_file_path = temp_path + os.sep + user_file_name
-local_file_path = temp_path + os.sep + local_file_name
-path_file_path = temp_path + os.sep + path_file_name
+user_file_name = 'user.txt'
+local_file_name = 'local.txt'
+path_file_name = 'path.txt'
+
+if (os.name == 'nt'):
+    temp_path = os.path.join(os.environ.get("APPDATA"), "thulearn2018")
+elif (os.name == 'posix'):
+    temp_path = os.path.join(os.environ.get(
+        "XDG_CONFIG_HOME", os.path.expanduser("~/.config")), "thulearn2018")
+else:
+    temp_path = Path.home()
+if (not os.path.exists(temp_path)):
+    os.makedirs(temp_path)
+
+user_file_path = os.path.join(temp_path, user_file_name)
+local_file_path = os.path.join(temp_path, local_file_name)
+path_file_path = os.path.join(temp_path, path_file_name)
 
 login_id_url = "https://id.tsinghua.edu.cn/do/off/ui/auth/login/post/bb5df85216504820be7bba2b0ae1535b/0?/login.do"
 login_url = url + "b/j_spring_security_thauth_roaming_entry"
@@ -29,40 +39,46 @@ upload_api = url + 'b/wlxt/kczy/zy/student/tjzy'
 def file_url(lesson_id, file_id):
     return url + "b/wlxt/kj/wlkc_kjxxb/student/kjxxb/" + lesson_id + "/" + file_id
 
+
 def lessons_url(semester):
     return url + "b/wlxt/kc/v_wlkc_xs_xkb_kcb_extend/student/loadCourseBySemesterId/" + semester + "/en"
+
 
 def download_before_url(fid):
     return url + "b/kc/wj_wjb/downloadFileBefore?wjid=" + fid
 
+
 def download_url(fid):
     return url + "b/wlxt/kj/wlkc_kjxxb/student/downloadFile?sfgk=0&wjid=" + fid
 
+
 def homeworks_url(lesson_id):
-	form = "aoData=[{\"name\":\"wlkcid\",\"value\":\"" + lesson_id + "\"}]"
-	return [ url + "b/wlxt/kczy/zy/student/zyListYjwg?" + form, \
-             url + "b/wlxt/kczy/zy/student/zyListWj?" + form]
+    form = "aoData=[{\"name\":\"wlkcid\",\"value\":\"" + lesson_id + "\"}]"
+    return [url + "b/wlxt/kczy/zy/student/zyListYjwg?" + form,
+            url + "b/wlxt/kczy/zy/student/zyListWj?" + form]
+
 
 def homework_url(lesson_id, hw):
     return url + "f/wlxt/kczy/zy/student/viewTj?wlkcid=" + lesson_id + "&sfgq=0&zyid=" + hw["zyid"] + "&xszyid=" + hw["xszyid"]
 
+
 def upload_form(homework_id, file_path, message):
     if (file_path == '.' + os.sep):
         return MultipartEncoder(
-            fields = {
-                'xszyid' : homework_id,
-                'isDeleted' : '1',
+            fields={
+                'xszyid': homework_id,
+                'isDeleted': '1',
                 'zynr': message
             },
-            boundary = '----WebKitFormBoundaryTytyPd5kgvE3t0kW'
+            boundary='----WebKitFormBoundaryTytyPd5kgvE3t0kW'
         )
     else:
         return MultipartEncoder(
-            fields = {
-                'fileupload' :(os.path.basename(file_path), open(file_path, 'rb'), 'application/octet-stream'),
-                'xszyid' : homework_id,
-                'isDeleted' : '0',
+            fields={
+                'fileupload': (os.path.basename(file_path), open(file_path, 'rb'), 'application/octet-stream'),
+                'xszyid': homework_id,
+                'isDeleted': '0',
                 'zynr': message
             },
-            boundary = '----WebKitFormBoundaryTytyPd5kgvE3t0kW'
+            boundary='----WebKitFormBoundaryTytyPd5kgvE3t0kW'
         )
