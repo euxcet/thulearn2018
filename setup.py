c0b68af31b99a0a6a5f8710ec755205f799ff97e
Yurui Hong <yuruihong02@outlook.com>
2024-04-18 20:21:53 +0800

    version 2.4.6

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="thulearn2018",
-    version="2.4.5",
+    version="2.4.6",
     author="Chengchi Zhou, Yingtian Liu, Yurui Hong",
     author_email="zcc16@mails.tsinghua.edu.cn, liu-yt16@mails.tsinghua.edu.cn,"
                  "yuruihong02@outlook.com",
